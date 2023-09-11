aab99877b9a5136b9ed461244d186c2d09286e72
GuadalupeBaigorria2 <guabaiegu@gmail.com>
2023-09-11 17:41:32 -0300

    Error People

--- source/Controllers/People.controller.js
+++ source/Controllers/People.controller.js
@@ -3,11 +3,11 @@ import { getConnection } from "../Database/Database.js";
 export const PostPerson= async(req, res)=> {
     // try {
         const{UserName, LastName, Edad}= req.body;
-        if(!! UserName && !! LastName && !!Edad)
-        {
+        // if(!! UserName && !! LastName && !!Edad)
+        // {
             const conexion= await getConnection();
-            conexion.query("insert into usuario set ?", {username,lastname,edad}).then("Se creo el usuario exitosamente");
-        }
+            conexion.query("insert into usuario set ?", {UserName,LastName,Edad}).then("Se creo el usuario exitosamente");
+        // }
     // } catch (error) {
     //     console.log(error);
     // }

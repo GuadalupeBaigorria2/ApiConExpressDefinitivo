3dec115032c0adc767a3f7deb05195c7bf67ffea
GuadalupeBaigorria2 <guabaiegu@gmail.com>
2023-09-11 18:07:28 -0300

    Error get

--- source/Controllers/People.controller.js
+++ source/Controllers/People.controller.js
@@ -7,8 +7,21 @@ export const PostPerson= async(req, res)=> {
         // {
             const conexion= await getConnection();
             conexion.query("insert into usuario set ?", {UserName,LastName,Edad}).then("Se creo el usuario exitosamente");
+            res.send("Exito");
         // }
     // } catch (error) {
     //     console.log(error);
     // }
+}
+// async va a esperar respuesta y no se va  terminar de ejecutar, esto corre mientras otros metodos corren
+export const GetPerson= async(req, res)=> {
+    try {
+        const edad= 5;
+        const conexion= await getConnection();
+        const [respuesta]= conexion.query("select * from usuario");
+        res.json(respuesta);
+
+    } catch (error) {
+        
+    }
 }
--- source/Routes/People.routes.js
+++ source/Routes/People.routes.js
@@ -1,9 +1,11 @@
-import { PostPerson } from "../Controllers/People.controller.js";
+import { PostPerson, GetPerson } from "../Controllers/People.controller.js";
 import { Router } from "express";
 
 //Con este Routes vamos a poder definir rutas que nos pasen
 const router= Router();
 
 router.post("/", PostPerson);
+router.get("/", GetPerson);
+
 export default router;
 
